874523be7a6a2041cf91809560c9513dada67172
jakelevi1996 <jakelevi@hotmail.co.uk>
2020-08-02 13:38:31 +0100

    Add comments and docstrings Set random seed Add a few extra test cases

--- Tests/test_network_errors.py
+++ Tests/test_network_errors.py
@@ -1,56 +1,96 @@
 """
 Module to test errors that can be raised by the NeuralNetwork class. TODO:
-docstrings and comments, also update docstrings for methods in NeuralNetwork
-class detailing the errors that are raised
+update docstrings for methods in NeuralNetwork class detailing the errors that
+are raised
 """
 
 import numpy as np
 import pytest
 from models import NeuralNetwork
+import activations as a
+import errors as e
 from .util import get_random_network
 
-def test_set_vector_wrong_size():
+# Set numpy random seed
+np.random.seed(1225)
+
+def test_set_vector_errors():
+    """
+    Test the errors raised by the NeuralNetwork.set_parameter_vector method when
+    there is an invalid argument
+    """
     n = get_random_network()
     num_params = n.get_parameter_vector().size
 
     with pytest.raises(ValueError):
+        # Wrong number of parameters
         n.set_parameter_vector(np.zeros(num_params - 3))
     
     with pytest.raises(ValueError):
+        # Parameter array has the wrong shape (should be 1-dimensional)
         n.set_parameter_vector(np.zeros([1, num_params]))
 
     with pytest.raises(AttributeError):
+        # Parameter vector is not a numpy array, so has no reshape method
         n.set_parameter_vector([0] * num_params)
 
 def test_invalid_act_function():
+    """
+    Test the errors raised by initialising an instance of the NeuralNetwork
+    class when there is an invalid value for the act_funcs argument
+    """
+    with pytest.raises(TypeError):
+        # act_funcs argument should be a list of activation function objects
+        n = NeuralNetwork(act_funcs=a.Gaussian())
+    
+    n = NeuralNetwork(act_funcs=[a.Gaussian])
+    N_D = 10
+    x = np.random.normal(size=[n.input_dim, N_D])
+    with pytest.raises(TypeError):
+        # Network is initialised with the Gaussian class, not an instance
+        n.forward_prop(x)
+
     n = NeuralNetwork(act_funcs=[None])
     N_D = 10
     x = np.random.normal(size=[n.input_dim, N_D])
-    t = np.random.normal(size=[n.output_dim, N_D])
-    
     with pytest.raises(TypeError):
+        # Activation function None is not callable
         n.forward_prop(x)
         
     n = NeuralNetwork(act_funcs=[sum])
     N_D = 10
     x = np.random.normal(size=[n.input_dim, N_D])
     t = np.random.normal(size=[n.output_dim, N_D])
-    
+    # Activation function sum is callable, so forward_prop is okay
     n.forward_prop(x)
-
     with pytest.raises(AttributeError):
+        # Activation function sum has no dydx method, so backprop fails
         n.back_prop(x, t)
 
 def test_invalid_error_function():
+    """
+    Test the errors raised by initialising an instance of the NeuralNetwork
+    class when there is an invalid value for the error_func argument
+    """
+    n = NeuralNetwork(error_func=e.SumOfSquares)
+    N_D = 10
+    x = np.random.normal(size=[n.input_dim, N_D])
+    t = np.random.normal(size=[n.output_dim, N_D])
+    with pytest.raises(TypeError):
+        # Network is initialised with the SumOfSquares class, not an instance
+        n.mean_error(t, x)
+
     n = NeuralNetwork(error_func=None)
     N_D = 10
     x = np.random.normal(size=[n.input_dim, N_D])
     t = np.random.normal(size=[n.output_dim, N_D])
     
     with pytest.raises(TypeError):
+        # Error function None is not callable
         n.mean_error(t, x)
         
     with pytest.raises(AttributeError):
+        # Error function None does not have a dydx method
         n.back_prop(x, t)
 
     n = NeuralNetwork(error_func=sum)
@@ -58,15 +98,25 @@ def test_invalid_error_function():
     x = np.random.normal(size=[n.input_dim, N_D])
     t = np.random.normal(size=[n.output_dim, N_D])
     
+    # Error function sum is callable, so mean_error is okay
     n.mean_error(t, x)
 
     with pytest.raises(AttributeError):
+        # Error function sum has no dydx method, so backprop fails
         n.back_prop(x, t)
 
-def test_print_weights_bad_file():
+def test_print_weights_grads_bad_file():
+    """
+    Test calling print_weights and print_grads methods with invalid file
+    arguments
+    """
     n = get_random_network()
     with pytest.raises(AttributeError):
+        # file argument should be a file object returned by the open function
         n.print_weights(file="filename")
+    with pytest.raises(AttributeError):
+        # file argument should be a file object returned by the open function
+        n.print_grads(file="filename")
 
 def test_print_grads_before_backprop_error():
     """
@@ -76,3 +126,12 @@ def test_print_grads_before_backprop_error():
     n = get_random_network()
     with pytest.raises(AttributeError):
         n.print_grads()
+
+def test_forward_prop_bad_input_shape():
+    pass
+
+def test_back_prop_bad_input_shape():
+    pass
+
+def test_back_prop_bad_target_shape():
+    pass
