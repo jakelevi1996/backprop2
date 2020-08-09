0673007c6917ebc256c656b04dff20d3e404f0dd
jakelevi1996 <jakelevi@hotmail.co.uk>
2020-08-09 17:52:46 +0100

    Parametrize tests using pytest decorator Fix typos in comments and docstrings

--- Tests/test_activations.py
+++ Tests/test_activations.py
@@ -1,13 +1,11 @@
 """
 Module containing unit tests for the activations module.
-
-TODO: write decorator in util module to parameterise unit tests with different
-activation functions, instead of repeated for-loops?
 """
 import os
+import pytest
 import numpy as np
 import activations as a
-from .util import get_random_network_inputs_targets, iterate_random_seeds
+from .util import get_random_network_inputs_targets
 
 # Define list of activation functions to be tested
 act_func_list = [
@@ -18,46 +16,48 @@ act_func_list = [
 current_dir = os.path.dirname(os.path.abspath(__file__))
 output_dir = os.path.join(current_dir, "Outputs")
 
-@iterate_random_seeds(2144, 6646, 9914)
-def test_shapes():
+@pytest.mark.parametrize("seed", [2144, 6646, 9914])
+@pytest.mark.parametrize("act_func", act_func_list)
+def test_shapes(seed, act_func):
     """
     Test that the output from each activation function and its derivatives are
     the correct shapes
 
     TODO: second derivatives
     """
-    for act_func in act_func_list:
-        _, x, _, _ = get_random_network_inputs_targets()
-        assert act_func(x).shape == x.shape
-        assert act_func.dydx(x).shape == x.shape
+    np.random.seed(seed)
+    _, x, _, _ = get_random_network_inputs_targets()
+    assert act_func(x).shape == x.shape
+    assert act_func.dydx(x).shape == x.shape
 
-@iterate_random_seeds(3995, 1218, 589)
-def test_propagation():
+@pytest.mark.parametrize("seed", [3995, 1218, 589])
+@pytest.mark.parametrize("act_func", act_func_list)
+def test_propagation(seed, act_func):
     """
     Test that the activation function can be used for forward and back
     propagation in a neural network model
     """
-    for act_func in act_func_list:
-        n, x, t, _ = get_random_network_inputs_targets(act_funcs=[act_func])
-        n.forward_prop(x)
-        n.back_prop(x, t)
+    np.random.seed(seed)
+    n, x, t, _ = get_random_network_inputs_targets(act_funcs=[act_func])
+    n.forward_prop(x)
+    n.back_prop(x, t)
 
-@iterate_random_seeds(3832, 4226, 1727)
-def test_act_func_id():
+@pytest.mark.parametrize("act_func", act_func_list)
+def test_act_func_id(act_func):
     """
-    Test that the activation function IDs are consistent
+    Test that the activation function IDs are self-consistent, IE that the
+    correct activation function is returned from its id
     """
-    # Check that you get the correct activation function back from its id
-    for act_func in act_func_list:
-        act_func_id = act_func.get_id_from_func()
-        act_func_from_id = a.ActivationFunction().get_func_from_id(act_func_id)
-        assert type(act_func_from_id) is type(act_func)
-    
-    # Check that all of the ids are unique
+    act_func_id = act_func.get_id_from_func()
+    act_func_from_id = a.get_func_from_id(act_func_id)
+    assert type(act_func_from_id) is type(act_func)
+
+def test_act_func_ids_unique():
+    """ Check that all of the activation function IDs are unique """
     id_list = [act_func.get_id_from_func() for act_func in act_func_list]
     assert len(set(id_list)) == len(id_list)
 
-@iterate_random_seeds(6309, 7639, 2532)
-def test_plotting():
-    for act_func in act_func_list:
-        act_func.plot(output_dir)
+@pytest.mark.parametrize("act_func", act_func_list)
+def test_plotting(act_func):
+    """ Test plotting method for each activation function """
+    act_func.plot(output_dir)
--- Tests/test_errors.py
+++ Tests/test_errors.py
@@ -5,6 +5,7 @@ TODO: write decorator in util module to parameterise unit tests with different
 error functions, instead of repeated for-loops?
 """
 import os
+import pytest
 import numpy as np
 import errors as e
 from .util import get_random_network_inputs_targets, iterate_random_seeds
@@ -16,47 +17,50 @@ error_func_list = [e.SumOfSquares()]
 current_dir = os.path.dirname(os.path.abspath(__file__))
 output_dir = os.path.join(current_dir, "Outputs")
 
-@iterate_random_seeds(2144, 6646, 9914)
-def test_shapes():
+@pytest.mark.parametrize("seed", [2144, 6646, 9914])
+@pytest.mark.parametrize("error_func", error_func_list)
+def test_shapes(seed, error_func):
     """
     Test that the output from each activation function and its derivatives are
     the correct shapes
 
     TODO: second derivatives
     """
-    for error_func in error_func_list:
-        n, x, t, N_D = get_random_network_inputs_targets()
-        y = n.forward_prop(x)
-        assert error_func(y, t).shape == (1, N_D)
-        assert error_func.dEdy(y, t).shape == (n.output_dim, N_D)
+    np.random.seed(seed)
+    n, x, t, N_D = get_random_network_inputs_targets()
+    y = n.forward_prop(x)
+    assert error_func(y, t).shape == (1, N_D)
+    assert error_func.dEdy(y, t).shape == (n.output_dim, N_D)
 
-@iterate_random_seeds(3995, 1218, 589)
-def test_propagation():
+@pytest.mark.parametrize("seed", [3995, 1218, 589])
+@pytest.mark.parametrize("error_func", error_func_list)
+def test_propagation(seed, error_func):
     """
-    Test that the activation function can be used for back propagation and
+    Test that the error function can be used for back propagation and
     calculating the mean error in a neural network model
     """
-    for error_func in error_func_list:
-        n, x, t, _ = get_random_network_inputs_targets(error_func=error_func)
-        n.back_prop(x, t)
-        n.mean_error(t, x)
+    np.random.seed(seed)
+    n, x, t, _ = get_random_network_inputs_targets(error_func=error_func)
+    n.back_prop(x, t)
+    n.mean_error(t, x)
 
-@iterate_random_seeds(3832, 4226, 1727)
-def test_act_func_id():
+@pytest.mark.parametrize("error_func", error_func_list)
+def test_error_func_id(error_func):
     """
-    Test that the activation function IDs are consistent
+    Test that the error function IDs are self-consistent, IE that the correct
+    error function is returned from its id
     """
-    # Check that you get the correct activation function back from its id
-    for error_func in error_func_list:
-        error_func_id = error_func.get_id_from_func()
-        error_func_from_id = e.ErrorFunction().get_func_from_id(error_func_id)
-        assert type(error_func_from_id) is type(error_func)
+    # Check that you get the correct error function back from its id
+    error_func_id = error_func.get_id_from_func()
+    error_func_from_id = e.ErrorFunction().get_func_from_id(error_func_id)
+    assert type(error_func_from_id) is type(error_func)
     
+def test_error_func_ids_unique():
+    """ Check that all of the error function IDs are unique """
     # Check that all of the ids are unique
     id_list = [error_func.get_id_from_func() for error_func in error_func_list]
     assert len(set(id_list)) == len(id_list)
 
-@iterate_random_seeds(6309, 7639, 2532)
-def test_plotting():
-    for error_func in error_func_list:
-        error_func.plot(output_dir)
+@pytest.mark.parametrize("error_func", error_func_list)
+def test_plotting(error_func):
+    error_func.plot(output_dir)
