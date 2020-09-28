4d8b572b338151db8a968311162987f03e8c9edd
jakelevi1996 <jakelevi@hotmail.co.uk>
2020-09-28 01:39:43 +0100

    Add empty prototype module for Taylor-Newton

---
+++ _optimisers/taylor_newton.py
@@ -0,0 +1,8 @@
+"""
+Optimisation routine based on a diagonal approximation of the Hessian matrix,
+using a first order Taylor expansion of the change in the gradient vector, with
+AR/MA/ARMA smoothing of diagonal terms
+"""
+
+def taylor_newton_optimiser():
+    raise NotImplementedError
