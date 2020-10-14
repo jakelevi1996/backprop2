dbf401b4be42ad8f17e1fd4eea3f53b37f2bc3b8
jakelevi1996 <jakelevi@hotmail.co.uk>
2020-10-14 23:07:51 +0100

    Add wrapper for modules in the _models directory

---
+++ models.py
@@ -0,0 +1,7 @@
+"""
+Wrapper for modules, classes and functions related to models, found in the
+_models directory. See unit tests and scripts for usage examples.
+"""
+from _models.neuralnetwork import NeuralNetwork
+from _models import activations
+from _models import errors
