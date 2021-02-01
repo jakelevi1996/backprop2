8db75183d31c671795aa8c8236d7ecdbe6ea546a
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-02-01 00:27:07 +0000

    Add comments to the test_find_best_parameters function

--- Tests/test_experiment.py
+++ Tests/test_experiment.py
@@ -17,39 +17,54 @@ def test_find_best_parameters(seed, plot):
     the best parameters. As an objective function we use the norm of the inputs,
     for which the best parameters are the ones which have the lowest absolute
     value, and check that the best values are found """
+    # Set the random seed
     np.random.seed(seed)
+    # Set the number of parameters
     if plot:
+        # If plotting, only use a small number of parameters to save time
         num_params = 3
     else:
+        # Otherwise use a larger random number of parameters
         num_params = np.random.randint(5, 10)
-    param_names = list(string.ascii_letters[:num_params])
+
+    # Define the function that will be called by the Experiment object
     def run_experiment(**kwargs):
+        """ Function that will be called by the Experiment object """
+        # Check that we have the right number of inputs
         assert len(kwargs) == num_params
+        # Return the norm of the input values
         return np.linalg.norm(list(kwargs.values()))
     
+    # Get the output directory name, and create it if it doesn't exist already
     test_output_dir = os.path.join(
         output_dir,
         "test_find_best_parameters, seed = %i" % seed
     )
     if not os.path.isdir(test_output_dir):
         os.makedirs(test_output_dir)
+    # Get the output filename and open it
     output_filename = os.path.join(test_output_dir, "output.txt")
-    f = open(output_filename, "w")
-    experiment = Experiment(run_experiment, test_output_dir, output_file=f)
-    
-    addp = lambda *args: experiment.add_parameter(Parameter(*args))
+    with open(output_filename, "w") as f:
+        # Initialise the experiment object
+        experiment = Experiment(run_experiment, test_output_dir, output_file=f)
+        
+        # Define shortcut expression for adding parameters
+        addp = lambda *args: experiment.add_parameter(Parameter(*args))
+        # Iterate through each parameter
+        for i in range(num_params):
+            # Choose a unique and valid parameter name
+            name = string.ascii_letters[i]
+            # Choose a random number of values, range of values, and default
+            num_values = np.random.randint(5, 10)
+            val_range = np.random.normal(size=num_values)
+            default = np.random.choice(val_range)
+            # Add the parameter to the Experiment object
+            addp(name, default, val_range)
 
-    for i in range(num_params):
-        name = string.ascii_letters[i]
-        num_values = np.random.randint(5, 10)
-        val_range = np.random.normal(size=num_values)
-        default = np.random.choice(val_range)
-        addp(name, default, val_range)
+        # Call the method to find the best parameter values
+        experiment.find_best_parameters(plot)
 
-    experiment.find_best_parameters(plot)
-
-    f.close()
-
+    # Iterate through each parameter in the Experiment object
     for param in experiment._param_list:
         # Find the minimum absolute parameter value
         min_abs_val = min(abs(x) for x in param.val_range)
