372fbdbe70894ccbf2949cb9abedf4697e807248
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-05-16 21:22:11 +0100

    Replace dataset_dict in Tests\test_batch_sizes.py with get_random_dataset Also reformat some docstrings

--- Tests/test_batch_sizes.py
+++ Tests/test_batch_sizes.py
@@ -1,38 +1,36 @@
-"""
-Unit tests for classes in the optimisers.batch module, including
-FullTrainingSet, ConstantBatchSize, and DynamicBatchSize
-"""
+""" Unit tests for classes in the optimisers.batch module, including
+FullTrainingSet, ConstantBatchSize, and DynamicBatchSize """
 import os
 import numpy as np
 import pytest
 import optimisers, data, models
-from .util import dataset_dict, get_random_network, get_output_dir
-from .util import set_random_seed_from_args
+from .util import (
+    get_output_dir,
+    set_random_seed_from_args,
+    get_random_dataset,
+    get_random_network,
+)
 
 # Get name of output directory, and create it if it doesn't exist
 output_dir = get_output_dir("Batch sizes")
 
-@pytest.mark.parametrize("dataset_key", dataset_dict.keys())
-def test_full_training_set_batch(dataset_key):
-    """
-    Test using the full training set as a batch with the
-    optimisers.batch.FullTrainingSet class
-    """
-    set_random_seed_from_args("test_full_training_set_batch", dataset_key)
-    dataset = dataset_dict[dataset_key]
+@pytest.mark.parametrize("repeat", range(3))
+def test_full_training_set_batch(repeat):
+    """ Test using the full training set as a batch with the
+    optimisers.batch.FullTrainingSet class """
+    set_random_seed_from_args("test_full_training_set_batch", repeat)
+    dataset = get_random_dataset()
     batch_getter = optimisers.batch.FullTrainingSet()
     x_batch, y_batch = batch_getter.get_batch(dataset)
     assert x_batch.shape == dataset.x_train.shape
     assert y_batch.shape == dataset.y_train.shape
 
-@pytest.mark.parametrize("dataset_key", dataset_dict.keys())
-def test_constant_batch_size(dataset_key):
-    """
-    Test using a constant batch size with the optimisers.batch.ConstantBatchSize
-    class
-    """
-    set_random_seed_from_args("test_constant_batch_size", dataset_key)
-    dataset = dataset_dict[dataset_key]
+@pytest.mark.parametrize("repeat", range(3))
+def test_constant_batch_size(repeat):
+    """ Test using a constant batch size with the
+    optimisers.batch.ConstantBatchSize class """
+    set_random_seed_from_args("test_constant_batch_size", repeat)
+    dataset = get_random_dataset()
     batch_size = np.random.randint(10, 20)
     batch_getter = optimisers.batch.ConstantBatchSize(batch_size)
     x_batch, y_batch = batch_getter.get_batch(dataset)
@@ -40,27 +38,23 @@ def test_constant_batch_size(dataset_key):
     assert y_batch.shape == (dataset.y_train.shape[0], batch_size)
 
 def test_constant_batch_size_non_integer_fail():
-    """
-    Test that using a non-integer batch size raises an exception when using the
-    optimisers.batch.ConstantBatchSize class
-    """
+    """ Test that using a non-integer batch size raises an exception when using
+    the optimisers.batch.ConstantBatchSize class """
     batch_size = 3.7
     with pytest.raises(TypeError):
         batch_getter = optimisers.batch.ConstantBatchSize(batch_size)
 
-@pytest.mark.parametrize("dataset_key", dataset_dict.keys())
-def test_dynamic_batch_size(dataset_key):
-    """
-    Test using a dynamic batch size with the
-    optimisers.batch.DynamicBatchSize class
-    """
-    set_random_seed_from_args("test_dynamic_batch_size", dataset_key)
-    dataset = dataset_dict[dataset_key]
-    batch_size = np.random.randint(10, 20)
+@pytest.mark.parametrize("repeat", range(3))
+def test_dynamic_batch_size(repeat):
+    """ Test using a dynamic batch size with the
+    optimisers.batch.DynamicBatchSize class """
+    set_random_seed_from_args("test_dynamic_batch_size", repeat)
+    dataset = get_random_dataset()
     model = models.NeuralNetwork(dataset.input_dim, dataset.output_dim)
+    batch_size = np.random.randint(10, 20)
     batch_getter = optimisers.batch.DynamicBatchSize(model, dataset)
     n_iters = np.random.randint(50, 100)
-    output_fname = "Test dynamic batch size, dataset = %s.txt" % dataset_key
+    output_fname = "test_dynamic_batch_size, dataset = %s.txt" % repr(dataset)
     with open(os.path.join(output_dir, output_fname), "w") as f:
         result = optimisers.Result(file=f)
         result.add_column(optimisers.results.columns.BatchSize(batch_getter))
