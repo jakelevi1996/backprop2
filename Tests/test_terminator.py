b633797a13190e1276e2491d70e596038e8a263f
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-07-18 23:55:05 +0100

    Add unit test for DynamicTerminator class

--- Tests/test_terminator.py
+++ Tests/test_terminator.py
@@ -7,7 +7,8 @@ import optimisers
 import models
 import data
 from .util import (
-    get_random_network_inputs_targets,
+    get_random_network,
+    get_random_dataset,
     get_output_dir,
     set_random_seed_from_args,
 )
@@ -25,3 +26,28 @@ def test_terminator(repeat):
         assert not terminator.ready_to_terminate(i)
     
     assert terminator.ready_to_terminate(num_iters)
+
+@pytest.mark.parametrize("repeat", [1, 2, 3])
+def test_dynamic_terminator(repeat):
+    set_random_seed_from_args("test_dynamic_terminator", repeat)
+    num_iters = np.random.randint(20, 50)
+    dataset = get_random_dataset()
+    model = get_random_network(
+        input_dim=dataset.input_dim,
+        output_dim=dataset.output_dim,
+    )
+    batch_size = np.random.randint(3, dataset.n_train)
+    dynamic_terminator = optimisers.DynamicTerminator(
+        model=model,
+        dataset=dataset,
+        batch_size=batch_size,
+        i_lim=num_iters,
+    )
+    dynamic_terminator.set_initial_iteration(0)
+    for i in range(num_iters):
+        x_batch, y_batch = dynamic_terminator.get_batch(dataset)
+        assert x_batch.shape == (dataset.input_dim, batch_size)
+        assert y_batch.shape == (dataset.output_dim, batch_size)
+        assert len(dynamic_terminator._p_improve_list) == (i + 1)
+    
+    assert dynamic_terminator.ready_to_terminate(num_iters)
