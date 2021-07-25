ef24e4af40c36a15a1292e339e3e8750ed9323cd
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-07-25 20:20:54 +0100

    Add configurable interval for recalculating probability of batch improvment Update to-do lists in docstrings

--- optimisers/terminator.py
+++ optimisers/terminator.py
@@ -5,10 +5,7 @@ from optimisers.batch import _BatchGetter
 class Terminator(TimedObject):
     """ The Terminator class is used to decide when to exit the main loop in
     the AbstractOptimiser.optimise method, based on either time, iteration
-    number, or error value.
-
-    TODO: add support for DBS
-    """
+    number, or error value """
     def __init__(self, t_lim=None, i_lim=None, e_lim=None):
         """ Initialise a Terminator object """
         self.t_lim = t_lim
@@ -51,8 +48,14 @@ class DynamicTerminator(Terminator, _BatchGetter):
     the training set. If this class is used as the terminator for optimisation,
     then it must also be used as the batch-getter.
 
-    TODO: implement dynamic batch sizing based on probability of improvement on
-    recently unseen points
+    TODO:
+    -   Implement dynamic batch sizing based on probability of improvement on
+        recently unseen points
+    -   Investigate having separate smoothers for mean and standard deviation
+        of the error, instead of one smoother for the probability of
+        improvement
+    -   Investigate smoothing the Boolean (in {-1, 1}) value of whether the
+        batch was an improvement
     """
     def __init__(
         self,
@@ -63,6 +66,7 @@ class DynamicTerminator(Terminator, _BatchGetter):
         smoother=None,
         t_lim=None,
         i_lim=None,
+        i_interval=5,
     ):
         self.t_lim = t_lim
         self._num_iterations = i_lim
@@ -78,7 +82,11 @@ class DynamicTerminator(Terminator, _BatchGetter):
         self._prev_mean_error = model.mean_error(dataset.y_train)
         self._model = model
         self._smoother = smoother
-        self._p_improve_list = [0]
+        self._p_improve_list = [1]
+
+        self._i_interval = i_interval
+        self._i_next_update = i_interval
+        self._i = 0
 
     def ready_to_terminate(self, i=None, error=None):
         """ Return True if ready to break out of the minimisation loop,
@@ -111,19 +119,23 @@ class DynamicTerminator(Terminator, _BatchGetter):
         x_batch = dataset.x_train[:, batch_inds]
         y_batch = dataset.y_train[:, batch_inds]
 
-        # Calculate the probability of improvement
-        self._model.forward_prop(x_batch)
-        error = self._model.error(y_batch)
-        mean_error = error.mean()
-        std_error = error.std()
-        p_improve = (
-            (self._prev_mean_error - mean_error) / std_error
-        )
-        if self._smoother is not None:
-            p_improve = self._smoother.smooth(p_improve)
+        if self._i > self._i_next_update:
+            # Calculate the probability of improvement
+            self._model.forward_prop(x_batch)
+            error = self._model.error(y_batch)
+            mean_error = error.mean()
+            std_error = error.std()
+            p_improve = (
+                (self._prev_mean_error - mean_error) / std_error
+            )
+            if self._smoother is not None:
+                p_improve = self._smoother.smooth(p_improve)
+            
+            self._p_improve_list.append(p_improve)
+            self._prev_mean_error = mean_error
+            self._i_next_update += self._i_interval
         
-        self._p_improve_list.append(p_improve)
-        self._prev_mean_error = mean_error
+        self._i += 1
 
         # Return the inputs and outputs for the new batch
         return x_batch, y_batch
