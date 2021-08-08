d1ebe233c2aed984ddf35156911830e1306d4060
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-08-08 19:02:43 +0100

    Add outline for Dinosaur class

---
+++ models/dinosaur/dinosaur.py
@@ -0,0 +1,13 @@
+""" This module contains the Dinosaur class for meta-learning """
+
+class Dinosaur:
+    """ Dinosaur class for meta-learning """
+
+    def __init__(self):
+        """ Initialise a dinosaur object """
+    
+    def meta_learn(self, task_set, terminator=None):
+        """ Learn meta-parameters for a task-set """
+    
+    def _fast_adapt(self, data_set):
+        """ Adapt to a data set, given the current meta-parameters """
