1ab6cfe7229e45a53f57808968284b2667f210dc
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-08-30 17:24:06 +0100

    Plot training tasks Set random seed for repeatable results

--- Scripts/train_dinosaur.py
+++ Scripts/train_dinosaur.py
@@ -1,9 +1,13 @@
+import os
 import numpy as np
 if __name__ == "__main__":
     import __init__
 import models
 import data
 import optimisers
+import plotting
+
+np.random.seed(0)
 
 # Define input and output dimensions for models and data
 input_dim = 2
@@ -18,10 +22,23 @@ network = models.NeuralNetwork(
 )
 dinosaur = models.Dinosaur(network)
 
+# Get directory for saving outputs to disk
+current_dir = os.path.dirname(os.path.abspath(__file__))
+output_dir = os.path.join(
+    current_dir,
+    "Outputs",
+    "Train Dinosaur",
+)
+if not os.path.isdir(output_dir):
+    os.makedirs(output_dir)
+print("Saving output plots in \"%s\"" % output_dir)
+os.system("explorer \"%s\"" % output_dir)
+
 # Initialise task set
 task_set = data.TaskSet()
 for x in [0.3, 0.9]:
     for y in [0.5, 0.7]:
+        # Initialise task
         task = data.GaussianCurve(
             input_dim=input_dim,
             output_dim=output_dim,
@@ -34,7 +51,16 @@ for x in [0.3, 0.9]:
             output_offset=0,
             output_scale=10,
         )
+        # Add task to task set
         task_set.add_task(task)
+        # Plot task for reference and save to disk
+        plotting.plot_2D_regression(
+            "Dinosaur task, x = %.1f, y = %.1f" % (x, y),
+            output_dir,
+            task,
+            output_dim,
+            model=network,
+        )
 
 for _ in range(10):
     # Perform one outer-loop iteration of meta-learning
