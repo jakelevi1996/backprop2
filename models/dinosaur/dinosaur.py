49b64884be5870a081e84da047e2cbc68dcbd1a9
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-08-30 17:06:04 +0100

    Initialise Dinosaur with a network object fast_adapt is now a public method so it can be used at test time for unseen tasks

--- models/dinosaur/dinosaur.py
+++ models/dinosaur/dinosaur.py
@@ -3,13 +3,14 @@
 class Dinosaur:
     """ Dinosaur class for meta-learning """
 
-    def __init__(self):
+    def __init__(self, network):
         """ Initialise a dinosaur object """
+        self._network = network
         self.mean = None
         self.scale = None
     
     def meta_learn(self, task_set, terminator=None):
         """ Learn meta-parameters for a task-set """
     
-    def _fast_adapt(self, data_set):
+    def fast_adapt(self, data_set):
         """ Adapt to a data set, given the current meta-parameters """
