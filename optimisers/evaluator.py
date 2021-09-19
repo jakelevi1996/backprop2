a90c914991f1a80f3d455b22dc5d4d55ce65258a
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-09-19 18:08:59 +0100

    Override the set_timer method in the Evaluator class so that the ready_to_evaluate method works as expected when re-entering an optimisation loop with the same Evaluator object

--- optimisers/evaluator.py
+++ optimisers/evaluator.py
@@ -8,7 +8,6 @@ class Evaluator(TimedObject):
         """ Initialise an Evaluator object """
         self.t_interval = t_interval
         self.i_interval = i_interval
-        self.t_next_print = 0
         self._init_timer()
     
     def set_initial_iteration(self, i):
@@ -29,8 +28,7 @@ class Evaluator(TimedObject):
             self.i_next_print = i + self.i_interval
     
     def ready_to_evaluate(self, i):
-        """
-        Return True if ready to evaluate the model, otherwise return False.
+        """ Return True if ready to evaluate the model, otherwise return False
         """
         if self.t_interval is not None:
             if self.time_elapsed() >= self.t_next_print:
@@ -43,6 +41,14 @@ class Evaluator(TimedObject):
                 return True
         
         return False
+
+    def set_timer(self, timer):
+        """ Set the timer for this object, and reset the t_next_print
+        attribute, so that the ready_to_evaluate method works as expected when
+        re-entering an optimisation loop with the same Evaluator object """
+        self._timer = timer
+        self.t_next_print = 0
+
 
 class DoNotEvaluate(Evaluator):
     """ Class for an evaluator which never evaluates """
