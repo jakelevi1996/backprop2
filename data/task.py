055c765e6b930a633483625f08385990ec211e73
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-10-24 15:09:01 +0100

    Add TaskSet get_batch method

--- data/task.py
+++ data/task.py
@@ -1,4 +1,4 @@
-""" Module containing the TaskSet class for meta-learning """
+import numpy as np
 
 class TaskSet:
     def __init__(self):
@@ -8,3 +8,12 @@ class TaskSet:
     def add_task(self, task):
         """ Add a task to this TaskSet object """
         self.task_list.append(task)
+
+    def get_batch(self, batch_size, replace=False):
+        batch_inds = np.random.choice(
+            len(self.task_list),
+            batch_size,
+            replace,
+        )
+        task_batch = [self.task_list[i] for i in batch_inds]
+        return task_batch
