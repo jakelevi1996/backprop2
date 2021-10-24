69c22574b87efa036f1b4039784bad626e788563
jakelevi1996 <jakelevi@hotmail.co.uk>
2021-10-24 15:09:23 +0100

    Add TaskSubMap and TaskMap classes

--- data/task.py
+++ data/task.py
@@ -1,10 +1,12 @@
+""" Module containing the TaskSet class for meta-learning, and the TaskMap and
+TaskSubMap classes for dynamic memory classification """
 import numpy as np
 
 class TaskSet:
     def __init__(self):
         """ Initialise a TaskSet object """
         self.task_list = []
-    
+
     def add_task(self, task):
         """ Add a task to this TaskSet object """
         self.task_list.append(task)
@@ -17,3 +19,23 @@ class TaskSet:
         )
         task_batch = [self.task_list[i] for i in batch_inds]
         return task_batch
+
+class TaskSubMap:
+    def __init__(self):
+        self.dict = dict()
+        self.num_tasks = 0
+
+    def add_task(self, task, label):
+        if label in self.dict:
+            self.dict[label].add_task(task)
+        else:
+            self.dict[label] = TaskSet()
+            self.dict[label].add_task(task)
+
+        self.num_tasks += 1
+
+class TaskMap:
+    def __init__(self):
+        self.train        = TaskSubMap()
+        self.test         = TaskSubMap()
+        self.validation   = TaskSubMap()
